6c9eb925850b7478577c1a57b66b565f44e2a8cf
Oleg Klimov <omgtech@gmail.com>
2025-10-03 07:28:43 +0200

    all_install.sh

---
+++ all_install.sh
@@ -0,0 +1,9 @@
+set -ex
+if [ -z "$1" ]; then
+    echo "Usage: $0 <workspace_id>"
+    exit 1
+fi
+WS_ID=$1
+python -m flexus_simple_bots.frog.frog_install --ws "$WS_ID"
+python -m flexus_simple_bots.slonik.slonik_install --ws "$WS_ID"
+python -m flexus_simple_bots.karen.karen_install --ws "$WS_ID"
